8553da1cd4b731cae53a2a345d395b1e13b42943
xstater <a166224@163.com>
2019-09-23 20:00:39 +0800

    add functions

--- File.h
+++ File.h
@@ -1,16 +1,31 @@
 #ifndef MAIEVERTIAS_FILE_H
 #define MAIEVERTIAS_FILE_H
 
+#include <exception>
 #include "Path.h"
 
 namespace maievertias{
     class File{
     public:
-        class FileIterator;
-
+        static File current();
+    
         File(const Path &);
         File(Path &&);
         ~File();
+        
+        enum class Type{
+            other,
+            regular,
+            directory,
+            fifo,
+            block,
+            character,
+            socket,
+            symlink,
+        }
+        
+        const Path &path()const noexcept;
+        Path &path()noexcept;
 
         bool isRegularFile()const noexcept;
         bool isDirectory()const noexcept;
@@ -18,17 +33,23 @@ namespace maievertias{
         bool isBlockFile()const noexcept;
         bool isCharacterFile()const noexcept;
         bool isSocket()const noexcept;
-        bool isEmpty()const noexcept;
+        //bool isEmpty()const noexcept;
         bool isSymlink()const noexcept;
         bool isOther()const noexcept;
 
         bool exist()const noexcept;
+        operator bool()const noexcept;
 
         std::uint32_t size()const noexcept;
-
-
+        
+        File copy(const Path &);
+        File &move(const Path &);
+        void remove();
+        void create(Type type);
     protected:
     private:
+        Type m_type;
+        Path m_path;
     };
 }
 
--- Path.h
+++ Path.h
@@ -2,17 +2,63 @@
 #define MAIEVERTIAS_PATH_H
 
 #include <string>
+#include <list>
+#include <iostream>
+
+/* Windows    C:\Users\fuck.txt
+ * Unix    \usr\fuck.txt
+ */
 
 namespace maievertias{
     class Path{
     public:
-        Path(const char *str);
-        ~Path();
-
+        using string_iterator = std::string::iterator;
         
+        class iterator{
+        public:
+            iterator(string_iterator itr,string_iterator end);
+            ~iterator();
+            
+            iterator &operator++()noexcept
+            const iterator operator++(int)noexcept;
+            
+            bool operator==()const noexcept;
+            bool operator!=()const noexcept;
+            
+            Path operator*()const noexcept;
+        protected:
+        private:
+            string_iterator m_itr;
+            string_iterator m_end;
+        }
+    
+        Path(const std::string &str);
+        Path(const Path &);
+        Path(Path &&);
+        ~Path();
+        
+        Path &operator=(const Path &);
+        Path &operator=(Path &&);
+        
+        const char *c_str()const noexcept;
+
+        Path name()const noexcept;
+        Path extension()const noexcept;
+        Path parent()const noexcept;
+        Path path()const noexcept;
+        Path stem()const noexcept;
+        Path root()const noexcept;
+        bool empty()const noexcept;
+        
+        iterator begin();
+        iterator end();
+        
+        friend std::ostream &operator<<(std::ostream &os,const Path &os);
+        friend std::istream &operator<<(std::istream &is,const Path &os);
     protected:
     private:
         std::string m_raw_path;
+        
     };
 }
 
