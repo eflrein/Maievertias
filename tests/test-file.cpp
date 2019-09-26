0f8800cff055264a5fc3b1e8e433e8beb112d392
xstater <a166224@163.com>
2019-09-27 00:09:40 +0800

    new functions test

--- tests/test-file.cpp
+++ tests/test-file.cpp
@@ -27,6 +27,29 @@ CASE_BEGIN(directory_iterator)
     }
 CASE_END
 
+CASE_BEGIN(move_test)
+    fs::File f(fs::Path(R"(D:\git\Maievertias\cmake-build-debug\asd)"));
+    ASSERT(f.exist(),"File not exist");
+    f.move(fs::Path(R"(D:\git\Maievertias\cmake-build-release\asd)"));
+    INFO("moved:",f.path());
+    ASSERT(f.exist(),"File not exist");
+CASE_END
+
+CASE_BEGIN(rename_test)
+    fs::File f(fs::Path(R"(D:\git\Maievertias\cmake-build-debug\asdasd.txt)"));
+    f.rename(fs::Path("asd.txt"));
+CASE_END
+
+CASE_BEGIN(delete_test)
+    fs::File f(fs::Path(R"(D:\git\Maievertias\cmake-build-debug\asdasd.txt)"));
+    ASSERT(f.exist(),"file not exist , cannot be deleted");
+    ASSERT(!f.remove().exist(),"File exist , delete failed");
+CASE_END
+
 RUN(file_exist_test,
     file_noexist_test,
-    directory_iterator)
+    directory_iterator
+    //move_test //passed,
+    //rename_test//passed
+    //delete_test //passed
+)
